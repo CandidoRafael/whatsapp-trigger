56ead33b7390a283e051cd0c2a8db818551d68d0
CandidoRafael <rafaeladelinoc@gmail.com>
2024-05-31 10:24:42 -0300

    add cors and vercel file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { sendWhatsAppMessage, connectToWhatsApp } = require('./config/WhatsAppClient');
-
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
